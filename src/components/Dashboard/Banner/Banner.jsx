5075b0a95a0da29ff04ddcb259095dbbc5d23e35
“kishlaychandan00” <kishlaychandan00@email.com>
2024-08-05 20:35:12 +0530

    updating Banner Image

--- src/components/Dashboard/Banner/Banner.jsx
+++ src/components/Dashboard/Banner/Banner.jsx
@@ -5,6 +5,7 @@ function Banner() {
   return (
     <>
     <div className={style.banner}>
+      <img src="https://static.vecteezy.com/system/resources/thumbnails/032/028/929/small/spices-and-herbs-with-measuring-spoons-photo.jpg" alt="" />
       <Link to={"/menu"} className={style.menuLink}>Explore Menu</Link>
     </div>
     </>
