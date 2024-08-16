80664481f87e7279efb67cafd502f351c36abf85
“kishlaychandan00” <kishlaychandan00@email.com>
2024-08-16 14:03:59 +0530

    updating env file

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,5 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+console.log("API_KEY");
 const API_KEY=import.meta.env.VITE_FIREBASE_API;
 console.log(API_KEY);
 const firebaseConfig = {
